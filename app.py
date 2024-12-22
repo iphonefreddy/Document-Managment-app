1cb34794e37a94cc45084f5f02944c9d962ca7c6
PolicyTeam-1 <hannahsunkissedfeet@gmail.com>
2024-12-21 21:32:32 -0700

    Update app.py
    Added new appy for admin

--- app.py
+++ app.py
@@ -15,7 +15,9 @@ class Policy(db.Model):
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullable=False)
     read = db.Column(db.Boolean, default=False)
-
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
 # Initialize the database
 with app.app_context():
     db.create_all()
