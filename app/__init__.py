e635328833ee4fd7031f71f5249a20906cb1ebb8
iphonefreddy <iphonefreddy@gmail.com>
2024-12-28 21:50:47 -0700

    Update __init__.py
    Moved new init into app file

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,22 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from flask_login import LoginManager
 
 db = SQLAlchemy()
-migrate = Migrate()
 login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'  # For local development
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
     app.config['SECRET_KEY'] = 'your_secret_key_here'
 
     db.init_app(app)
-    migrate.init_app(app, db)
     login_manager.init_app(app)
 
+    # Import and register routes
     with app.app_context():
-        from . import routes, models
+        from . import routes
         db.create_all()
 
     return app
